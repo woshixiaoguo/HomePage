9d49ed8c2e01acffa43c524f210529c67220d5b9
Apollo <isguohr@qq.com>
2024-05-16 20:30:48 +0800

    first upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,72 @@
-const gulp = require('gulp')
-const minifycss = require('gulp-clean-css')
-const uglify = require('gulp-uglify')
-const htmlmin = require('gulp-htmlmin')
-const cssnano = require('gulp-cssnano')
-const htmlclean = require('gulp-htmlclean')
-const del = require('del')
-const babel = require('gulp-babel')
-const autoprefixer = require('gulp-autoprefixer')
-const connect = require('gulp-connect')
-const pug = require('gulp-pug')
-const sass = require('gulp-sass')
-sass.compiler = require('node-sass')
+const gulp = require("gulp");
+const minifycss = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const htmlmin = require("gulp-htmlmin");
+const cssnano = require("gulp-cssnano");
+const htmlclean = require("gulp-htmlclean");
+const del = require("del");
+const babel = require("gulp-babel");
+const autoprefixer = require("gulp-autoprefixer");
+const connect = require("gulp-connect");
+const pug = require("gulp-pug");
+// const sass = require('gulp-sass')
+const sass = require("gulp-sass")(require("node-sass"));
+// sass.compiler = require("node-sass");
 
-const config = require('./config.json')
+const config = require("./config.json");
 
-gulp.task('clean', function () {
-	return del(['./dist/css/', './dist/js/'])
-})
+gulp.task("clean", function () {
+	return del(["./dist/css/", "./dist/js/"]);
+});
 
-gulp.task('css', function () {
+gulp.task("css", function () {
 	return gulp
-		.src('./src/css/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(minifycss({ compatibility: 'ie8' }))
-		.pipe(autoprefixer({ browsers: ['last 2 version'] }))
+		.src("./src/css/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(minifycss({ compatibility: "ie8" }))
+		.pipe(autoprefixer({ browsers: ["last 2 version"] }))
 		.pipe(cssnano({ reduceIdents: false }))
-		.pipe(gulp.dest('./dist/css'))
-})
+		.pipe(gulp.dest("./dist/css"));
+});
 
-gulp.task('html', function () {
+gulp.task("html", function () {
 	return gulp
-		.src('./dist/index.html')
+		.src("./dist/index.html")
 		.pipe(htmlclean())
 		.pipe(htmlmin())
-		.pipe(gulp.dest('./dist'))
-})
+		.pipe(gulp.dest("./dist"));
+});
 
-gulp.task('js', function () {
+gulp.task("js", function () {
 	return gulp
-		.src('./src/js/*.js')
-		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.src("./src/js/*.js")
+		.pipe(babel({ presets: ["@babel/preset-env"] }))
 		.pipe(uglify())
-		.pipe(gulp.dest('./dist/js'))
-})
+		.pipe(gulp.dest("./dist/js"));
+});
 
-gulp.task('pug', function () {
+gulp.task("pug", function () {
 	return gulp
-		.src('./src/index.pug')
+		.src("./src/index.pug")
 		.pipe(pug({ data: config }))
-		.pipe(gulp.dest('./dist'))
-})
+		.pipe(gulp.dest("./dist"));
+});
 
-gulp.task('assets', function () {
-	return gulp
-		.src(['./src/assets/**/*'])
-		.pipe(gulp.dest('./dist/assets'));
-})
+gulp.task("assets", function () {
+	return gulp.src(["./src/assets/**/*"]).pipe(gulp.dest("./dist/assets"));
+});
 
-gulp.task('build', gulp.series('clean', 'assets', 'pug', 'css', 'js', 'html'))
-gulp.task('default', gulp.series('build'))
+gulp.task("build", gulp.series("clean", "assets", "pug", "css", "js", "html"));
+gulp.task("default", gulp.series("build"));
 
-gulp.task('watch', function () {
-	gulp.watch('./src/components/*.pug', gulp.parallel('pug'))
-	gulp.watch('./src/index.pug', gulp.parallel('pug'))
-	gulp.watch('./src/css/**/*.scss', gulp.parallel(['css']))
-	gulp.watch('./src/js/*.js', gulp.parallel(['js']))
+gulp.task("watch", function () {
+	gulp.watch("./src/components/*.pug", gulp.parallel("pug"));
+	gulp.watch("./src/index.pug", gulp.parallel("pug"));
+	gulp.watch("./src/css/**/*.scss", gulp.parallel(["css"]));
+	gulp.watch("./src/js/*.js", gulp.parallel(["js"]));
 	connect.server({
-		root: 'dist',
+		root: "dist",
 		livereload: true,
-		port: 8080
-	})
-})
+		port: 9000,
+	});
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,86 +1,88 @@
 window.hiddenProperty =
-	'hidden' in document
-		? 'hidden'
-		: 'webkitHidden' in document
-			? 'webkitHidden'
-			: 'mozHidden' in document
-				? 'mozHidden'
-				: null
+	"hidden" in document
+		? "hidden"
+		: "webkitHidden" in document
+		? "webkitHidden"
+		: "mozHidden" in document
+		? "mozHidden"
+		: null;
 
 window.DIRECTIONS = {
-	UP: 'UP',
-	DOWN: 'DOWN',
-	LEFT: 'LEFT',
-	RIGHT: 'RIGHT',
-	UNDIRECTED: 'UNDIRECTED'
-}
+	UP: "UP",
+	DOWN: "DOWN",
+	LEFT: "LEFT",
+	RIGHT: "RIGHT",
+	UNDIRECTED: "UNDIRECTED",
+};
 window.isPhone =
 	/Mobile|Android|iOS|iPhone|iPad|iPod|Windows Phone|KFAPWI/i.test(
 		navigator.userAgent
-	)
+	);
 
 function getMoveDirection(startx, starty, endx, endy) {
 	if (!isPhone) {
-		return
+		return;
 	}
 
-	const angx = endx - startx
-	const angy = endy - starty
+	const angx = endx - startx;
+	const angy = endy - starty;
 
 	if (Math.abs(angx) < 2 && Math.abs(angy) < 2) {
-		return DIRECTIONS.UNDIRECTED
+		return DIRECTIONS.UNDIRECTED;
 	}
 
-	const getAngle = (angx, angy) => (Math.atan2(angy, angx) * 180) / Math.PI
+	const getAngle = (angx, angy) => (Math.atan2(angy, angx) * 180) / Math.PI;
 
-	const angle = getAngle(angx, angy)
+	const angle = getAngle(angx, angy);
 	if (angle >= -135 && angle <= -45) {
-		return DIRECTIONS.UP
+		return DIRECTIONS.UP;
 	} else if (angle > 45 && angle < 135) {
-		return DIRECTIONS.DOWN
+		return DIRECTIONS.DOWN;
 	} else if (
 		(angle >= 135 && angle <= 180) ||
 		(angle >= -180 && angle < -135)
 	) {
-		return DIRECTIONS.LEFT
+		return DIRECTIONS.LEFT;
 	} else if (angle >= -45 && angle <= 45) {
-		return DIRECTIONS.RIGHT
+		return DIRECTIONS.RIGHT;
 	}
 
-	return DIRECTIONS.UNDIRECTED
+	return DIRECTIONS.UNDIRECTED;
 }
 
 function loadIntro() {
-	if (document[hiddenProperty] || loadIntro.loaded) { return }
-	
+	if (document[hiddenProperty] || loadIntro.loaded) {
+		return;
+	}
+
 	setTimeout(() => {
-		$('.wrap').classList.add('in')
+		$(".wrap").classList.add("in");
 		setTimeout(() => {
-			$('.content-subtitle').innerHTML = `<span>${[...subtitle].join(
-				'</span><span>'
-			)}</span>`
-		}, 270)
-	}, 0)
-	loadIntro.loaded = true
+			$(".content-subtitle").innerHTML = `<span>${[...subtitle].join(
+				"</span><span>"
+			)}</span>`;
+		}, 270);
+	}, 0);
+	loadIntro.loaded = true;
 }
 
 function switchPage() {
 	if (switchPage.switched) {
-		return
+		return;
 	}
 	const DOM = {
-		intro: $('.content-intro'),
-		path: $('.shape-wrap path'),
-		shape: $('svg.shape')
-	}
-	DOM.shape.style.transformOrigin = '50% 0%'
+		intro: $(".content-intro"),
+		path: $(".shape-wrap path"),
+		shape: $("svg.shape"),
+	};
+	DOM.shape.style.transformOrigin = "50% 0%";
 
 	anime({
 		targets: DOM.intro,
 		duration: 1100,
-		easing: 'easeInOutSine',
-		translateY: '-200vh'
-	})
+		easing: "easeInOutSine",
+		translateY: "-200vh",
+	});
 
 	anime({
 		targets: DOM.shape,
@@ -88,88 +90,71 @@ function switchPage() {
 			{
 				value: [0.8, 1.8],
 				duration: 550,
-				easing: 'easeInQuad'
+				easing: "easeInQuad",
 			},
 			{
 				value: 1,
 				duration: 550,
-				easing: 'easeOutQuad'
-			}
-		]
-	})
+				easing: "easeOutQuad",
+			},
+		],
+	});
 	anime({
 		targets: DOM.path,
 		duration: 1100,
-		easing: 'easeOutQuad',
-		d: DOM.path.getAttribute('pathdata:id'),
+		easing: "easeOutQuad",
+		d: DOM.path.getAttribute("pathdata:id"),
 		complete: function (anim) {
 			if (canvas) {
-				cancelAnimationFrame(animationID)
-				canvas.parentElement.removeChild(canvas)
-				canvas = null
+				cancelAnimationFrame(animationID);
+				canvas.parentElement.removeChild(canvas);
+				canvas = null;
 			}
-		}
-	})
+		},
+	});
 
-	switchPage.switched = true
+	switchPage.switched = true;
 }
-
 
 function loadMain() {
 	if (loadMain.loaded) {
-		return
+		return;
 	}
 	setTimeout(() => {
-		$('.card-inner').classList.add('in')
-	}, 400)
-	loadMain.loaded = true
-}
-
-function loadAll() {
-	if (loadAll.loaded) {
-		return
-	}
-	switchPage()
-	loadMain()
-	loadAll.loaded = true
+		$(".card-inner").classList.add("in");
+	}, 400);
+	loadMain.loaded = true;
 }
 
 window.visibilityChangeEvent = hiddenProperty.replace(
 	/hidden/i,
-	'visibilitychange'
-)
-window.addEventListener(visibilityChangeEvent, loadIntro)
-window.addEventListener('DOMContentLoaded', loadIntro)
-
-const enterEl = $('.enter')
-enterEl.addEventListener('click', loadAll)
-enterEl.addEventListener('touchenter', loadAll)
-
-document.body.addEventListener('mousewheel', loadAll, { passive: true })
-$('.arrow').addEventListener('mouseenter', loadAll)
+	"visibilitychange"
+);
+window.addEventListener(visibilityChangeEvent, loadIntro);
+window.addEventListener("DOMContentLoaded", loadIntro);
 
 if (isPhone) {
 	document.addEventListener(
-		'touchstart',
+		"touchstart",
 		function (e) {
-			window.startx = e.touches[0].pageX
-			window.starty = e.touches[0].pageY
+			window.startx = e.touches[0].pageX;
+			window.starty = e.touches[0].pageY;
 		},
 		{ passive: true }
-	)
+	);
 	document.addEventListener(
-		'touchend',
+		"touchend",
 		function (e) {
-			let endx, endy
-			endx = e.changedTouches[0].pageX
-			endy = e.changedTouches[0].pageY
+			let endx, endy;
+			endx = e.changedTouches[0].pageX;
+			endy = e.changedTouches[0].pageY;
 
-			const direction = getMoveDirection(startx, starty, endx, endy)
+			const direction = getMoveDirection(startx, starty, endx, endy);
 			if (direction !== DIRECTIONS.UP) {
-				return
+				return;
 			}
-			loadAll()
+			loadAll();
 		},
 		{ passive: true }
-	)
+	);
 }
